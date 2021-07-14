bf53e80c0901c0c593dcd845c892f2998632d2b7
Abilio Fernandes da Silva <69885511+datascientistabilio@users.noreply.github.com>
2021-07-13 23:11:59 -0300

    Create iniciante.py

---
+++ iniciante/iniciante.py
@@ -0,0 +1,130 @@
+#Hellow world cod 1000
+print("Hello World!")
+
+#Soma cod 1001
+a = int(input())
+b = int(input())
+X = a + b
+print("X =", X)
+
+#Área do circulo cod 1002
+n=3.14159
+raio = float(input())
+print(f"A={n*(raio*raio):.4f}")
+
+#Soma simples cod 1003
+A = int(input())
+B = int(input())
+SOMA = A+B
+print("SOMA =",SOMA)
+
+#Produto simples cod 1004
+A = int(input())
+B = int(input())
+PROD = A*B
+print("PROD =",PROD)
+
+#Média simples cod 1005
+A = float(input())
+B = float(input())
+MEDIA = (A*3.5+B*7.5)/11
+print(f"MEDIA = {MEDIA:.5f}")
+
+#Média com 3 variáveis cod 1006
+A = float(input())
+B = float(input())
+C = float(input())
+MEDIA = (A*2+B*3+C*5)/10
+print(f"MEDIA = {MEDIA:.1f}")
+
+#Diferença cod 1007
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+DIFERENCA = (A*B-C*D)
+print("DIFERENCA =",DIFERENCA)
+
+#Calculado o salário por hora trabalhada cod 1008
+NUMBER = int(input())
+HOURS = int(input())
+VALUE = float(input())
+SALARY = HOURS*VALUE
+print("NUMBER =",NUMBER)
+print(f"SALARY = U$ {SALARY:.2f}")
+
+#SÁLARIO COM BONUS cod 1009
+NOME = input()
+SALARIO = float(input())
+TOTAL_VENDAS = float(input())
+print(f"TOTAL = R$ {TOTAL_VENDAS*0.15+SALARIO:.2f}")
+
+#CALCULO SIMPLES cod 1010
+linha1 = input().split(" ")
+linha2 = input().split(" ")
+
+cod1, qtde1, valor1 = linha1
+cod2, qtde2, valor2 = linha2
+
+total = (int(qtde1) * float(valor1)) + (int(qtde2) * float(valor2))
+
+print("VALOR A PAGAR: R$ %0.2f" %total)
+
+# Calculando o volume cod 1011
+raio = float(input())
+pi = 3.14159
+
+print(f"VOLUME = {4/3*pi*raio**3:.3f}")
+
+#Área cod 1012
+linha = input().split(" ")
+a, b, c = linha
+print(f"TRIANGULO: {float(a)*float(c)/2:.3f}")
+print(f"CIRCULO: {3.14159*float(c)**2:.3f}")
+print(f"TRAPEZIO: {((float(a)+float(b))/2)*float(c):.3f}")
+print(f"QUADRADO: {float(b)*float(b):.3f}")
+print(f"RETANGULO: {float(a)*float(b):.3f}")
+
+# cod 1013
+
+
+# cod 1014
+
+
+# cod 1015
+
+
+# cod 1016
+
+
+# cod 1017
+
+
+# cod 1018
+
+
+# cod 1019
+
+
+
+# cod 1020
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
