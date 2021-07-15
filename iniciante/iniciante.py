84e47f65fd7a95318df227183618d92d09254754
Abilio Fernandes da Silva <69885511+datascientistabilio@users.noreply.github.com>
2021-07-15 15:01:51 -0300

    Update iniciante.py

--- iniciante/iniciante.py
+++ iniciante/iniciante.py
@@ -131,12 +131,33 @@ segundo = num
 print('{}:{}:{}'.format(hora, minuto, segundo))
 
 
-# cod 1020
+# Idade em dias cod 1020
+dia = int(input())
+ano = int(dia/365)
+dia -= ano*365
+meses = int(dia/30)
+dia -= meses*30
+print(f'{ano} ano(s)')
+print(f'{meses} mes(es)')
+print(f'{dia} dia(s)')
 
+# Notas e Moedas 1021
+valor = float(input())
 
+notas = [100, 50, 20, 10, 5, 2]
+moedas = [1, 0.50, 0.25, 0.10, 0.05, 0.01]
 
+print("NOTAS:")
+for nota in notas:
+    qtd_cedulas = int(valor / nota)
+    print("{} nota(s) de R$ {:.2f}".format(qtd_cedulas, nota))
+    valor -= qtd_cedulas * nota
 
-
+print("MOEDAS:")
+for moeda in moedas:
+    qtd_moedas = int(round(valor,2) / moeda)
+    print("{} moeda(s) de R$ {:.2f}".format(qtd_moedas, moeda))
+    valor -= round(qtd_moedas * moeda, 2)
 
 
 
