77f4a35c214ec28040f48a0cc695a83b5666caba
Abilio Fernandes da Silva <69885511+datascientistabilio@users.noreply.github.com>
2021-07-17 16:10:50 -0300

    Update iniciante.py

--- iniciante/iniciante.py
+++ iniciante/iniciante.py
@@ -229,7 +229,327 @@ if hora_inicial == hora_final:
 
 print('O JOGO DUROU {} HORA(S) E {} MINUTO(S)'.format(h, m))
 
+# 1052
+mes = int(input())
 
+if mes == 1:
+    print("January")
+elif mes == 2:
+    print("February")
+elif mes == 3:
+    print("March")
+elif mes == 4:
+    print("April")
+elif mes == 5:
+    print("May")
+elif mes == 6:
+    print("June")
+elif mes == 7:
+    print("July")
+elif mes == 8:
+    print("August")
+elif mes == 9:
+    print("September")
+elif mes == 10:
+    print("October")
+elif mes == 11:
+    print("November")
+elif mes == 12:
+    print("December")
 
+# 1066
+par=0
+imp=0
+pos=0
+neg = 0
+for contador in range(0,5,1):
+    num = int(input())    
+    if num%2==0:
+        par+= 1
+    else:
+        imp+= 1
+    if num>0:
+        pos+= 1
+    if num<0:
+        neg+= 1
 
+print(par,"valor(es) par(es)")
+print(imp,"valor(es) impar(es)")
+print(pos,"valor(es) positivo(s)")
+print(neg,"valor(es) negativo(s)")
 
+# média 3 1040
+num1,num2,num3,num4 = list(map(float, input().split(" ")))
+media = ((num1*2)+(num2*3)+(num3*4)+(num4*1))/(2+3+4+1)
+if media >= 7:
+    print(f"Media: {media:.1f}")
+    print("Aluno aprovado.")
+elif media <5:
+    print(f"Media: {media:.1f}")
+    print("Aluno reprovado.")
+elif media >=5 and media <=6.9:
+    print(f"Media: {media:.1f}")
+    print("Aluno em exame.")
+    exame = float(input())
+    print("Nota do exame:",exame)
+    nota_recup = (exame+media)/2
+    if nota_recup >= 5:
+        print("Aluno aprovado.")
+        print(f"Media final: {nota_recup:.1f}")
+    else:
+        print("Aluno reprovado.",nota_recup)
+
+# 1067
+num = int(input())
+x=0
+for x in range(num):
+    
+    if x%2==1:
+      print(x)
+if num%2==1:
+  print(num)
+  
+  
+# 1048
+sal = float(input())
+
+if sal >=0 and sal <=400:
+    reajuste = sal*0.15 
+    sal = (sal*0.15)+sal
+    print(f"Novo salario: {sal:.2f}")
+    print(f"Reajuste ganho: {reajuste:.2f}")
+    print(f"Em percentual: 15 %")
+elif sal >= 400.01 and sal <= 800:
+    reajuste = sal*0.12
+    sal = (sal*0.12)+sal
+    print(f"Novo salario: {sal:.2f}")
+    print(f"Reajuste ganho: {reajuste:.2f}")
+    print(f"Em percentual: 12 %")
+elif sal >= 800.01 and sal <= 1200:
+    reajuste = sal*0.10
+    sal = (sal*0.10)+sal
+    print(f"Novo salario: {sal:.2f}")
+    print(f"Reajuste ganho: {reajuste:.2f}")
+    print(f"Em percentual: 10 %")
+elif sal >= 1200.01 and sal <= 2000:
+    reajuste = sal*0.07
+    sal = (sal*0.07)+sal
+    print(f"Novo salario: {sal:.2f}")
+    print(f"Reajuste ganho: {reajuste:.2f}")
+    print(f"Em percentual: 7 %")
+elif sal > 2000:
+    reajuste = sal*0.04
+    sal = (sal*0.04)+sal
+    print(f"Novo salario: {sal:.2f}")
+    print(f"Reajuste ganho: {reajuste:.2f}")
+    print(f"Em percentual: 4 %")
+    
+# ERRO ERRO 2686 FIX
+while True: 
+    try: 
+        minut9o = float(input())
+        if minuto == 360 or minuto >=0 and minuto<= 105:
+            print("Bom Dia!!")
+            if minuto == 0 or minuto == 360:
+                  print("06:00:00")
+            if minuto >0 and minuto <=15:
+                
+                resp = minuto-0
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("06:0"+str(minuto)+":00")
+                else:
+                  print("06:"+str(minuto)+":00")
+            elif minuto >15 and minuto <=30:
+                resp = minuto-15
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("07:0"+str(minuto)+":00")
+                else:
+                  print("07:"+str(minuto)+":00")
+            elif minuto >30 and minuto <=45:
+                resp = minuto-30
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("08:0"+str(minuto)+":00")
+                else:
+                  print("08:"+str(minuto)+":00")
+            elif minuto >45 and minuto <=60:
+                resp = minuto-45
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("09:0"+str(minuto)+":00")
+                else:
+                  print("09:"+str(minuto)+":00")
+            elif minuto >60 and minuto <=75:
+                resp = minuto-60
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("10:0"+str(minuto)+":00")
+                else:
+                  print("10:"+str(minuto)+":00")
+            elif minuto >75 and minuto <90:
+                resp = minuto-75
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("11:0"+str(minuto)+":00")
+                else:
+                  print("11:"+str(minuto)+":00")
+            elif minuto >90 and minuto <=105:
+                resp = minuto-90
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("12:0"+str(minuto)+":00")
+                else:
+                  print("12:"+str(minuto)+":00")
+        elif minuto >105 and minuto<=195:
+            print("Boa Tarde!!")
+            if minuto >105 and minuto <=120:
+                resp = minuto-105
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("13:0"+str(minuto)+":00")
+                else:
+                  print("13:"+str(minuto)+":00")
+            elif minuto >120 and minuto <=135:
+                resp = minuto-120
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("14:0"+str(minuto)+":00")
+                else:
+                  print("14:"+str(minuto)+":00")
+            elif minuto >135 and minuto <=150:
+                resp = minuto-135
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("15:0"+str(minuto)+":00")
+                else:
+                  print("15:"+str(minuto)+":00")
+            elif minuto >150 and minuto <=165:
+                resp = minuto-150
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("16:0"+str(minuto)+":00")
+                else:
+                  print("16:"+str(minuto)+":00")
+            elif minuto >165 and minuto <=180:
+                resp = minuto-165
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("17:0"+str(minuto)+":00")
+                else:
+                  print("17:"+str(minuto)+":00")
+            elif minuto >180 and minuto <=195:
+                resp = minuto-180
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("18:0"+str(minuto)+":00")
+                else:
+                  print("18:"+str(minuto)+":00")
+        elif minuto >195 and minuto<=285:
+            print("Boa Noite!!")
+            if minuto >195 and minuto <=210:
+                resp = minuto-195
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("19:0"+str(minuto)+":00")
+                else:
+                  print("19:"+str(minuto)+":00")
+            elif minuto >210 and minuto <=225:
+                resp = minuto-210
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("20:0"+str(minuto)+":00")
+                else:
+                  print("20:"+str(minuto)+":00")
+            elif minuto >225 and minuto <=240:
+                resp = minuto-225
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("21:0"+str(minuto)+":00")
+                else:
+                  print("21:"+str(minuto)+":00")
+            elif minuto >240 and minuto <=255:
+                resp = minuto-240
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("22:0"+str(minuto)+":00")
+                else:
+                  print("22:"+str(minuto)+":00")
+            elif minuto >255 and minuto <=270:
+                resp = minuto-255
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("23:0"+str(minuto)+":00")
+                else:
+                  print("23:"+str(minuto)+":00")
+            elif minuto >270 and minuto <=285:
+                resp = minuto-270
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("00:0"+str(minuto)+":00")
+                else:
+                  print("00:"+str(minuto)+":00")
+        elif minuto >285 and minuto<=360:
+            print("De Madrugada!!")
+            if minuto >285 and minuto <=299:
+                resp = minuto-285
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("01:0"+str(minuto)+":00")
+                else:
+                  print("01:"+str(minuto)+":00")
+            elif minuto >=300 and minuto <=315:
+                resp = minuto-300
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("02:0"+str(minuto)+":00")
+                else:
+                  print("02:"+str(minuto)+":00")
+            elif minuto >315 and minuto <=330:
+                resp = minuto-315
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("03:0"+str(minuto)+":00")
+                else:
+                  print("03:"+str(minuto)+":00")
+            elif minuto >330 and minuto <=345:
+                resp = minuto-330
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("04:0"+str(minuto)+":00")
+                else:
+                  print("04:"+str(minuto)+":00")
+            elif minuto >345 and minuto <=359:
+                resp = minuto-345
+                resp = int(resp*4)
+                minuto = resp
+                if resp >=0 and resp <= 9:
+                  print("05:0"+str(minuto)+":00")
+                else:
+                  print("05:"+str(minuto)+":00")
+    except EOFError:
+                break
